389963ab82379270aa982f3b22b932dfd8f66003
Alpvax <development@alpvax.uk>
2023-01-06 12:29:16 +0000

    Add date input

--- web/src/views/GameView.vue
+++ web/src/views/GameView.vue
@@ -18,10 +18,16 @@
         <label :for="'select_' + player[1]">{{ player[0] }}</label>
       </div>
     </fieldset>
+    <input
+      id="dateInput"
+      v-model="date"
+      type="datetime-local"
+    >
   </div>
   <div class="game">
     <Game27
       :players="all_players.filter(([_name, id]) => players.includes(id))"
+      :date="new Date(date)"
     />
   </div>
 </template>
@@ -45,6 +51,7 @@ export default defineComponent({
     return {
       players,
       all_players,
+      date: ref((new Date()).toISOString().slice(0, 16)),
     };
   },
 });
@@ -60,5 +67,11 @@ export default defineComponent({
 }
 .game {
   font-size: 2.5vh;
+}
+.playerSelect fieldset {
+  display: inline-block;
+}
+#dateInput {
+  font-size: 2vh;
 }
 </style>
