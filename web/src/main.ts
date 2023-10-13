0635d59df3be80914697789e1010ee1e42303595
Alpvax <development@alpvax.uk>
2023-10-13 13:08:58 +0100

    format changes

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { initializeApp } from "firebase/app";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+initializeApp({
+  apiKey: "AIzaSyDlxprk1nL3NfhOjvvv8t7b_849NS9hndA",
+  authDomain: "alpvax-darts-scores.firebaseapp.com",
+  projectId: "alpvax-darts-scores",
+  storageBucket: "alpvax-darts-scores.appspot.com",
+  messagingSenderId: "747461320838",
+  appId: "1:747461320838:web:31f1c7482bbfb1a001b107",
+  measurementId: "G-YBGZQ1456K",
+});
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
