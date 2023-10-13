ce74b3f0792be9372e6d4cd12b3df0a47202cedc
Alpvax <development@alpvax.uk>
2023-10-13 12:46:08 +0100

    Init new web app, using vite

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,29 +1,14 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import './assets/main.css'
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import App from './App.vue'
+import router from './router'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDlxprk1nL3NfhOjvvv8t7b_849NS9hndA",
-  authDomain: "alpvax-darts-scores.firebaseapp.com",
-  projectId: "alpvax-darts-scores",
-  storageBucket: "alpvax-darts-scores.appspot.com",
-  messagingSenderId: "747461320838",
-  appId: "1:747461320838:web:31f1c7482bbfb1a001b107",
-  measurementId: "G-YBGZQ1456K",
-};
+const app = createApp(App)
 
-// Initialize Firebase
-initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,48 +1,23 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import GameView from "../views/GameView.vue";
-
-const routes: RouteRecordRaw[] = [
-  {
-    path: "/",
-    name: "game",
-    component: GameView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/history",
-    name: "history",
-    component: () => import(/* webpackChunkName: "history" */ "../views/HistoryView.vue"),
-  },
-  {
-    path: "/game/:gameId",
-    name: "pastGame",
-    props: true,
-    component: () => import(/* webpackChunkName: "prevGame" */ "../views/PastGameView.vue"),
-  },
-  {
-    path: "/summaryV2",
-    component: () => import(/* webpackChunkName: "summaryV2" */ "../views/SummaryView.vue"),
-  },
-  {
-    path: "/summaryV3",
-    component: () => import(/* webpackChunkName: "summaryV3" */ "../views/SummaryView3.vue"),
-  },
-  {
-    path: "/v2game",
-    component: () => import(/* webpackChunkName: "gameV2" */ "../views/GameView2.vue"),
-  },
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
 
-export default router;
+export default router
