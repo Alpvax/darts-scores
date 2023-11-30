713c253fc38b386fa027cc3b2ec12a936ed5717a
Alpvax <development@alpvax.uk>
2023-11-30 17:49:17 +0000

    Add summaryAccumulatorFactory

--- web/src/gameUtils/summary/index.ts
+++ web/src/gameUtils/summary/index.ts
@@ -25,7 +25,10 @@ export type PlayerDataForStats<T extends TurnData<any, any>> = {
   tied: string[];
 };
 
-export interface SummaryEntryField<T extends TurnData<any, any>, E, S> {
+interface SummaryValueRecord {
+  [key: string]: SummaryValueRecord | number;
+}
+export interface SummaryEntryField<T extends TurnData<any, any>, E, S extends SummaryValueRecord> {
   /** Create an entry for a single game */
   //TODO: use PlayerData instead of inline type declaration
   entry(playerData: PlayerDataForStats<T>, opponents: string[], tiebreakWinner?: string): E;
@@ -44,6 +47,123 @@ export type SummaryEntry<
   [key: string]: SummaryEntryField<T, any, any>;
 };
 
-export type SummaryValues<E extends SummaryEntry<T>, T extends TurnData<any, any>> = {
+export type SummaryValues<
+  E extends SummaryEntry<T, P>,
+  T extends TurnData<any, any>,
+  P extends PlayerRequirements = { all: "*" },
+> = {
   [K in keyof E]: E[K] extends SummaryEntryField<T, any, infer S> ? S : never;
+} & {
+  numGames: number;
 };
+
+const calcDeltaVal = <V extends number | SummaryValueRecord>(prev: V, val: V): V => {
+  if (typeof val === "number" && typeof prev === "number") {
+    return (val - prev) as V;
+  } else {
+    return Object.keys(val).reduce(
+      (acc, k) =>
+        Object.assign(acc, {
+          [k]: calcDeltaVal((prev as SummaryValueRecord)[k], (val as SummaryValueRecord)[k]),
+        }),
+      {} as V,
+    );
+  }
+};
+
+export const summaryAccumulatorFactory =
+  <
+    S extends SummaryEntry<T, P>,
+    T extends TurnData<any, any>,
+    P extends PlayerRequirements = { all: "*" },
+  >(
+    fields: S,
+  ) =>
+  (): {
+    /**
+     * @param playerData the data for the curent player
+     * @param allPlayers the ids of all the players in the game
+     * @param tiebreakWinner the player who won the tiebreak, if applicable
+     * @returns The changes to all values between the previous game and this one
+     */
+    addGame(
+      playerData: PlayerDataForStats<T>,
+      allPlayers: string[],
+      tiebreakWinner?: string,
+    ): SummaryValues<S, T, P>;
+    /**
+     * @param playerData the data for the curent player
+     * @param allPlayers the ids of all the players in the game
+     * @param tiebreakWinner the player who won the tiebreak, if applicable
+     * @returns The changes to all values between the previous game and this one. Does not modify the summary
+     */
+    getDeltas(
+      playerData: PlayerDataForStats<T>,
+      allPlayers: string[],
+      tiebreakWinner?: string,
+    ): SummaryValues<S, T, P>;
+    /** The current summary values. SHOULD NOT BE OVERWRITTEN OR MODIFIED! */
+    summary: SummaryValues<S, T, P>;
+  } => {
+    type SVTyp = SummaryValues<S, T, P>;
+    const fieldEntries = Object.entries(fields) as [keyof S, SummaryEntryField<T, any, any>][];
+    const summary = fieldEntries.reduce(
+      (summary, [k, field]) =>
+        Object.assign(summary, {
+          [k]: field.emptySummary(),
+        }),
+      { numGames: 0 } as SVTyp,
+    );
+    return {
+      addGame: (pData, players, tbWinner) => {
+        summary.numGames += 1;
+        return fieldEntries.reduce(
+          (deltas, [key, field]) => {
+            const entry = field.entry(
+              pData,
+              players.filter((pid) => pid !== pData.playerId),
+              tbWinner,
+            );
+            // Deep clone previous value
+            const prev = JSON.parse(JSON.stringify(summary[key]));
+            summary[key] = field.summary(prev, summary.numGames, entry);
+            deltas[key] = Object.entries(summary[key]).reduce(
+              (acc, [k, v]) =>
+                Object.assign(acc, {
+                  [k]: calcDeltaVal(prev[k], v),
+                }),
+              {} as SVTyp[typeof key],
+            );
+            return deltas;
+          },
+          { numGames: 1 } as SVTyp,
+        );
+      },
+      getDeltas: (pData, players, tbWinner) => {
+        return fieldEntries.reduce(
+          (deltas, [key, field]) => {
+            const entry = field.entry(
+              pData,
+              players.filter((pid) => pid !== pData.playerId),
+              tbWinner,
+            );
+            // Deep clone previous value
+            const prev = JSON.parse(JSON.stringify(summary[key]));
+            const newSummary: SVTyp[typeof key] = field.summary(prev, summary.numGames + 1, entry);
+            deltas[key] = Object.entries(newSummary).reduce(
+              (acc, [k, v]) =>
+                Object.assign(acc, {
+                  [k]: calcDeltaVal(prev[k], v),
+                }),
+              {} as SVTyp[typeof key],
+            );
+            return deltas;
+          },
+          { numGames: 1 } as SVTyp,
+        );
+      },
+      summary,
+    };
+  };
+
+export default summaryAccumulatorFactory;
