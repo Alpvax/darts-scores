93fccc4da1914168e22757f8431ffbd762041969
Alpvax <development@alpvax.uk>
2024-07-18 19:14:48 +0100

    Move GameResult out of rounds
    Remove "old approach"

---
+++ web/src/gameDefinitionV2/gameResult.ts
@@ -0,0 +1,65 @@
+import type { FixedLengthArray } from "@/utils/types";
+
+export type PlayerGameData<Turns, Extra extends {} = {}> = {
+  playerId: string;
+  // complete: boolean;
+  turns: Turns;
+  displayName?: string;
+  // handicap?: Handicap<Turns>
+} & Extra;
+
+export type GameResult<PData extends PlayerGameData<any, any>, PlayerId extends string = string> = {
+  date: Date;
+  playerOrder: PlayerId[];
+  tiebreak?: {
+    players: PlayerId[];
+    type: string;
+    winner: PlayerId;
+  };
+  results: {
+    [K in PlayerId]: PData;
+  };
+};
+
+export type PlayerStats<Turns, TurnStats, GameStats extends {}> = {};
+
+export type ArrayGameStats<Turns extends any[], TurnStats, GameStats extends {}> = GameStats & {
+  rounds: { [K in keyof Turns]: TurnStats };
+};
+
+export type GameStatsFactory<GR extends GameResult<any, any>, Stats> =
+  GR extends GameResult<PlayerGameData<infer Turns, infer Extra>, any>
+    ? (turns: Turns, extra: Extra) => Stats
+    : never;
+
+type Test27Plyr = PlayerGameData<
+  FixedLengthArray<0 | 1 | 2 | 3, 20>,
+  {
+    jesus?: boolean;
+  }
+>;
+type Test27 = GameResult<Test27Plyr>;
+type Stats27 = ArrayGameStats<
+  Test27Plyr["turns"],
+  {
+    cliff: boolean;
+    dd: boolean;
+    hits: 0 | 1 | 2 | 3;
+  },
+  {
+    fatNicks: boolean;
+    dream: boolean;
+    allPos: boolean;
+    hans: number;
+    goblins: boolean;
+    piranhas: boolean;
+    jesus: boolean;
+  }
+>;
+
+// const statsFactory27: GameStatsFactory<Test27, Stats27> = (turns, extra) => ({
+//   rounds:
+// })
+
+type TestBEPlyr = PlayerGameData<(0 | 1 | 2 | 3 | 5 | 6)[]>;
+type TestBE = GameResult<TestBEPlyr>;
--- web/src/gameDefinitionV2/index.ts
+++ web/src/gameDefinitionV2/index.ts
@@ -5,16 +5,6 @@ export type GameDefinitionCore<GameId extends string, PlayerState> = {
   gameId: GameId;
   makeInitialState: (playerId: string) => PlayerState;
 };
-export type GameDefinition<
-  PlayerState,
-  RoundIdx extends number,
-  RoundValues extends { [K in RoundIdx]: any },
-  RoundStats extends { [K in RoundIdx]: any },
-  RoundValsMut extends { [K in RoundIdx]?: RoundValues[K] },
-  GameId extends string = string,
-> = GameDefinitionCore<GameId, PlayerState> & {
-  rounds: { [K in RoundIdx]: TurnMeta<RoundValues[K], RoundStats[K], RoundValsMut[K]> };
-};
 
 type _WARFCurried<GameId extends string, PlayerState, V, Stats> = {
   <Len extends number>(
@@ -172,146 +162,52 @@ type ObjectGameDef<
   rounds: Rounds;
 };
 
-type FixedArrayGameDef<
-  PlayerState,
-  Len extends number,
-  V,
-  Stats,
-  VMut extends V | undefined,
-  GameId extends string = string,
-> =
-  NumericRange<Len> extends infer RoundIdx extends number
-    ? GameDefinition<
-        PlayerState,
-        RoundIdx,
-        { [K in RoundIdx]: V },
-        { [K in RoundIdx]: Stats },
-        { [K in RoundIdx]: VMut },
-        GameId
-      >
-    : never;
-
-/**
- * Curried function to allow specifying some types while inferring others.
- * Usually the first call (gameId) is inferred, second (playerState, turnValue, turnStats) typed, and third (round count) inferred
- * @param gameType the identifier of the game
- * @returns a function to create the game start state
+/*
+ * ============================================================================
+ * TEMPORARY TEST TYPES AND VALUES
+ * ============================================================================
  */
-export const defineFixedArrayGame =
-  <GameId extends string = string>(gameType: GameId) =>
-  /**
-   * @param makeInitialState the factory used to create the initial state for each player
-   * @returns a function to create the game rounds
-   */
-  <PlayerState, V, Stats>(makeInitialState: (playerId: string) => PlayerState) =>
-  /**
-   * @param length the length of the rounds array
-   * @param turnFactory the factory function to create the individual rounds
-   * @returns the GameDefinition
-   */
-  <Len extends number>(
-    length: Len,
-    turnFactory: (index: NumericRange<Len>) => TurnMetaDef<V, Stats>,
-  ) =>
-    ({
-      gameType,
-      makeInitialState,
-      // @ts-expect-error
-      rounds: Array.from({ length }, (_, idx) => defineTurn(turnFactory(idx as NumericRange<Len>))),
-    }) as TurnMetaDefLookup<ReturnType<typeof turnFactory>> extends TurnMeta<any, any, infer VMut>
-      ? FixedArrayGameDef<PlayerState, Len, V, Stats, VMut, GameId>
-      : unknown;
-/**
- * Version of {@link defineFixedArrayGame} without the currying
- * @param gameType the identifier of the game
- * @param makeInitialState the factory used to create the initial state for each player
- * @param length the length of the rounds array
- * @param turnFactory the factory function to create the individual rounds
- * @returns the `GameDefinition` (as a `FixedArrayGameDef`)
- */
-export const defineFixedArrayGameFull = <
-  PlayerState,
-  Len extends number,
-  V,
-  Stats,
-  GameId extends string = string,
->(
-  gameType: GameId,
-  makeInitialState: (playerId: string) => PlayerState,
-  length: Len,
-  turnFactory: (index: NumericRange<Len>) => TurnMetaDef<V, Stats>,
-) =>
-  // @ts-ignore
-  defineFixedArrayGame(gameType)(makeInitialState)(length, turnFactory);
 
-const gameType27v1 = defineFixedArrayGame("twentyseven")<
-  { score: number; jesus?: boolean },
-  NumericRange<4>,
-  { cliff: boolean; dd: boolean; hits: number }
->(() => ({ score: 27 }))(20, (idx) => ({
-  untakenValue: 0, // as NumericRange<4>,
-  deltaScore: (val) => {
-    switch (val) {
-      case 0:
-        return idx * -2;
-      case 1:
-        return idx * 2;
-      case 2:
-        return idx * 4;
-      case 3:
-        return idx * 6;
-    }
-  },
-  turnStats: (val) => ({
-    cliff: val === 3,
-    dd: val >= 2,
-    hits: val,
-  }),
-  component: (val, { deltaScore, score, mutable, focus }) => "TODO: component" /*{
-    mutable: (val, { deltaScore, score, mutable, focus }) => "TODO: mutable cell",
-    immutable: (val, { deltaScore, score, mutable, focus }) => "TODO: immutable cell",
-  }//*/,
-}));
-
-const gameType27v2 = gameDefinitionBuilder("twentyseven")<{ score: number; jesus?: boolean }>(
-  () => ({ score: 27 }),
-).withArrayRounds<NumericRange<4>, { cliff: boolean; dd: boolean; hits: number }>()(20, (idx) => ({
-  untakenValue: 0, // as NumericRange<4>,
-  deltaScore: (val) => {
-    switch (val) {
-      case 0:
-        return idx * -2;
-      case 1:
-        return idx * 2;
-      case 2:
-        return idx * 4;
-      case 3:
-        return idx * 6;
-    }
-  },
-  turnStats: (val) => ({
-    cliff: val === 3,
-    dd: val >= 2,
-    hits: val,
-  }),
-  component: (val, { deltaScore, score, mutable, focus }) => "TODO: component" /*{
+const gameType27 = gameDefinitionBuilder("twentyseven")<{ score: number; jesus?: boolean }>(() => ({
+  score: 27,
+})).withArrayRounds<NumericRange<4>, { cliff: boolean; dd: boolean; hits: number }>()(
+  20,
+  (idx) => ({
+    untakenValue: 0, // as NumericRange<4>,
+    deltaScore: (val) => {
+      switch (val) {
+        case 0:
+          return idx * -2;
+        case 1:
+          return idx * 2;
+        case 2:
+          return idx * 4;
+        case 3:
+          return idx * 6;
+      }
+    },
+    turnStats: (val) => ({
+      cliff: val === 3,
+      dd: val >= 2,
+      hits: val,
+    }),
+    component: (val, { deltaScore, score, mutable, focus }) => "TODO: component" /*{
       mutable: (val, { deltaScore, score, mutable, focus }) => "TODO: mutable cell",
       immutable: (val, { deltaScore, score, mutable, focus }) => "TODO: immutable cell",
     }//*/,
-}));
+  }),
+);
 
-type T27GameDefTarget = FixedArrayGameDef<
+type T27GameDefTarget = ArrayGameDef<
+  "twentyseven",
   { score: number; jesus?: boolean },
-  20,
   NumericRange<4>,
   { cliff: boolean; dd: boolean; hits: number },
   NumericRange<4>,
-  "twentyseven"
+  20
 >;
-type T27GameDefRetV1 = typeof gameType27v1;
-type T27GameDefRetV2 = typeof gameType27v2;
-type RetRoundsV1 = T27GameDefRetV1["rounds"];
-type RetRoundsV2 = T27GameDefRetV2["rounds"];
+type T27GameDefRet = typeof gameType27;
+type RetRoundsV1 = T27GameDefRet["rounds"];
 type TrgRounds = T27GameDefTarget["rounds"];
 
 type Cmp<A, B> = A extends B
@@ -322,36 +218,12 @@ type Cmp<A, B> = A extends B
     ? "B = A, A ! B"
     : false;
 
-type T27GameDefCmp1 = Cmp<T27GameDefTarget, T27GameDefRetV1>;
-type T27GameDefCmp2 = Cmp<T27GameDefTarget, T27GameDefRetV2>;
-type T27DetailedCmp1 = {
-  [K in keyof T27GameDefTarget]: Cmp<T27GameDefTarget[K], T27GameDefRetV1[K]> extends infer C
+type T27GameDefCmp = Cmp<T27GameDefTarget, T27GameDefRet>;
+type T27DetailedCmp = {
+  [K in keyof T27GameDefTarget]: Cmp<T27GameDefTarget[K], T27GameDefRet[K]> extends infer C
     ? C extends true
       ? true
-      : [C, T27GameDefTarget[K], T27GameDefRetV1[K]]
+      : [C, T27GameDefTarget[K], T27GameDefRet[K]]
     : never;
-};
-type T27DetailedCmp2 = {
-  [K in keyof T27GameDefTarget]: Cmp<T27GameDefTarget[K], T27GameDefRetV2[K]> extends infer C
-    ? C extends true
-      ? true
-      : [C, T27GameDefTarget[K], T27GameDefRetV2[K]]
-    : never;
-};
-type T27DetailedCmpRet = {
-  [K in keyof T27GameDefRetV1 | keyof T27GameDefRetV2]: K extends keyof T27GameDefRetV1
-    ? Cmp<T27GameDefRetV1[K], T27GameDefRetV2[K]> extends infer C
-      ? C extends true
-        ? true
-        : [C, T27GameDefTarget[K], T27GameDefRetV1[K]]
-      : never
-    : `key "${K}" does not exist in T27GameDefRetV1`;
 };
 type T27RoundsCmp = Cmp<TrgRounds, RetRoundsV1>;
-type T27RetRoundsCmp = {
-  [K in keyof RetRoundsV1]: Cmp<RetRoundsV1, RetRoundsV2> extends infer C
-    ? C extends true
-      ? true
-      : [C, RetRoundsV1[K], RetRoundsV2[K]]
-    : never;
-};
